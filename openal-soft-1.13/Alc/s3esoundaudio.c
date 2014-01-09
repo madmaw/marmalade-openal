31f21ddcdd1eaf4d32e46687e6d67fb5b976ed7a
Vedran Manojlovic <vmanojlo@gmail.com>
2014-01-09 01:27:56 +0100

    New version (as of January 2014) with new thread synchronizing algorithm. It uses less CPU, and it should work without any cracking sound. Throughly tested on all iOS mobile devices (iPhone/iPad/iPod), and on PC and on Android. It should work on other platforms too.

--- openal-soft-1.13/Alc/s3esoundaudio.c
+++ openal-soft-1.13/Alc/s3esoundaudio.c
@@ -30,14 +30,12 @@ typedef struct _s3e_data {
     // Stereo sound - use uint32
     ALubyte*                preBufferData;
     // Maximum size of pre-buffer data
+    // (number of int32 for stereo, or int16 for mono sound)
     int                     preBufferSize;
     // Pointer where is start of usable data
     volatile int            preBuffer_DataStartIndex;
     // How much usable (already mixed) data is in the buffer
-    volatile int            preBuffer_DataLength;
-    // If true, it means that data in pre-buffer is ready
-    // to be send to sound channel
-    volatile int            preBuffer_IsDataReady;
+    volatile int            preBuffer_DataEndIndex;
 
     // Pointer to worker thread
     // It has to be volatile, it can change in different threads
@@ -51,12 +49,19 @@ typedef struct _s3e_data {
     volatile int            thread_exited;
 } s3e_data;
 
+int getDataInBuffer( const int startIndex, const int endIndex, const int size )
+{
+    return startIndex <= endIndex   // also if equal - it means 0 length
+        ? endIndex - startIndex
+        : (size - startIndex) + endIndex;
+}
+
 
 static ALuint s3e_channel_thread( ALvoid *ptr )
 {
     ALCdevice *Device = (ALCdevice*)ptr;
     s3e_data *data = (s3e_data*)Device->ExtraData;
-    int samplesToMix, idx1, len1, len2;
+    int startIdx, samplesToMix, len, idx1, len1, len2;
     data->thread_exited = 0;
 
     // This thread will pre-mix sound data into internal buffer
@@ -64,51 +69,40 @@ static ALuint s3e_channel_thread( ALvoid *ptr )
     // copy data from that buffer... And then we get signal
     // to pre-mix some more data for next s3e sound callback.
 
-    // It is very important that this thread reacts ASAP to
-    // signal from s3e callback, so the main s3e thread is
-    // not paused for too long.
-    // Luckily, we can use semaphore to wait for signal from
-    // s3e callback function, that way we react almost immediately.
-
+    // This thread can only modify preBuffer_DataEndIndex and never preBuffer_DataStartIndex
     while( !data->killNow && !s3eDeviceCheckQuitRequest() )
     {
-        assert( data->preBuffer_IsDataReady == 0 );
-
-        if( data->preBuffer_DataLength == 0 ) data->preBuffer_DataStartIndex = 0;
+        startIdx = data->preBuffer_DataStartIndex;
+        len = getDataInBuffer( startIdx, data->preBuffer_DataEndIndex, data->preBufferSize );
+        assert( startIdx >= 0 && startIdx < data->preBufferSize );
+        assert( len >= 0 && len < data->preBufferSize );
 
         // Mix the data in the circular (ring) buffer
-        //samplesToMix = data->preBufferSize - data->preBuffer_DataLength;    // Always fill buffer to the max
-        samplesToMix = min( data->sampleSize, data->preBufferSize - data->preBuffer_DataLength );
+        // Always fill buffer to the max, minus 16 bytes
+        // Those 16 bytes we leave is so startIndex never equals endIndex
+        samplesToMix = min( 2*data->sampleSize, (data->preBufferSize - len) - 16 );
 
         // Mix data in two steps - first mix from the end of the buffer
-        idx1 = (data->preBuffer_DataStartIndex + data->preBuffer_DataLength);
-        if( idx1 >= data->preBufferSize ) idx1 -= data->preBufferSize;
+        idx1 = data->preBuffer_DataEndIndex;
         len1 = min( data->preBufferSize - idx1, samplesToMix );
         if( len1 > 0 ) aluMixData( Device, data->preBufferData + idx1*data->bytesPerSample, len1 );
         // Mix 2nd step - from start of the buffer
         len2 = samplesToMix - len1;
         if( len2 > 0 ) aluMixData( Device, data->preBufferData, len2 );
-        assert( len2 <= data->preBuffer_DataStartIndex );
+        assert( len2 <= startIdx );
 
+        // Ok, sound data mixed, move endIndex pointer
+        idx1 = data->preBuffer_DataEndIndex + samplesToMix;
+        if( idx1 >= data->preBufferSize ) idx1 -= data->preBufferSize;
+        data->preBuffer_DataEndIndex = idx1;
 
-        // Ok, sound data mixed
-        data->preBuffer_DataLength += samplesToMix;
-
-        // Mark it that we have data ready
-        data->preBuffer_IsDataReady = 1;
-
-        // Wait for s3e main thread to signal us via semaphore
-        // before we start mixing again
-        s3eDeviceYield(0);
+        // Wait until some of the data in buffer is used
+        // by s3e sound callback
         while( !data->killNow && s3eThreadSemWait(data->thread_semaphore, 10) != S3E_RESULT_SUCCESS )
         {
-            s3eDeviceYield(0);
-
             // Waited 10ms, but without any signal - just check if we need
             // to close this thread, and then wait again
             if( s3eDeviceCheckQuitRequest() ) data->killNow = 1;
-
-            // Wait another 10ms for signal from s3e callback
         }
     }
 
@@ -125,10 +119,9 @@ int32 s3e_more_audio( void* systemData, void* userData )
     // s3e main thread, and it has to wait for us to exit this 
     // function before it can call it again
     // NOTE: Inside this function we can't call s3eDeviceYield()
-    // or any other sleep function, because if we do, s3e sound
-    // callback 
+    // or any other sleep function (Marmalade limitation)
 
-    int dataWritten, len1, len2;
+    int dataLen, dataWritten, len1, len2;
     uint64 startTime;
     ALCdevice *pDevice = (ALCdevice*)userData;
     s3eSoundGenAudioInfo* info = (s3eSoundGenAudioInfo*)systemData;
@@ -137,17 +130,14 @@ int32 s3e_more_audio( void* systemData, void* userData )
     if( systemData == NULL || userData == NULL ) return 0;
     data = (s3e_data*)pDevice->ExtraData;
 
-    assert( (data->isStereo!=0) == (info->m_Stereo!=0) );
-
     // Check if this channel is actually closed or should be closed
     if( data == NULL || data->killNow || data->thread == NULL || data->thread_exited )
     {
+        data->killNow = 1;
         info->m_EndSample = S3E_TRUE;
         // If we would return 0 - as we should - s3e will not detect that
         // application is closed and will call our function again immediately.
         // So we return info->m_NumSamples so application will be closed properly.
-        // We clear output sound buffer so it would at least play silence.
-        //memset( info->m_Target, 0, info->m_NumSamples * data->bytesPerSample );
         return info->m_NumSamples;
         // We can't return 0 even though the app should be closed.
         // It would actually crash the app as s3e will delete 
@@ -157,44 +147,21 @@ int32 s3e_more_audio( void* systemData, void* userData )
         //return 0;
     }
 
+    assert( (data->isStereo!=0) == (info->m_Stereo!=0) );
+    assert( data->preBuffer_DataStartIndex >= 0 && data->preBuffer_DataStartIndex < data->preBufferSize );
 
-    // Check if mixed data is ready from worker thread
-    startTime = s3eTimerGetMs();
-    while( !data->preBuffer_IsDataReady ) 
+    // Check how much data is pre-buffered
+    dataLen = getDataInBuffer( data->preBuffer_DataStartIndex, data->preBuffer_DataEndIndex, data->preBufferSize );
+    if( dataLen == 0 )
     {
-        // No data is ready; but we CAN'T use s3eDeviceYield() nor Sleep()
-        // because that could call s3e_more_audio while we're already in it
-        // and that would break the code
-        //s3eDeviceYield(0);
-
-        // If we are waiting for worker thread to finish mixing
-        // for more than 20ms, it most probably means that application
-        // is paused (suspended) or has received terminate signal
-        // So don't wait any more
-        if( s3eTimerGetMs() - startTime > 20 )
-        {
-            // We HAVE to return info->m_NumSamples instead of 0
-            // even though we haven't mixed any data. This is because
-            // Marmalade doesn't detect that application is paused/closed
-            // and will keep calling our callback function until it gets
-            // all the data it required.
-            // So at least clear output sound buffer so it would play silence.
-            //memset( info->m_Target, 0, info->m_NumSamples * data->bytesPerSample );
-            return info->m_NumSamples;
-            //return 0; // We can't return 0 - it would block the application
-        }
-
-        // Check if application has received quit request or is paused
-        // Unfortunately, this doesn't always work - that's why we have
-        // timer above which will exit this function if more than 20ms passed
-        // If any of those two functions return true, it means that
-        // Marmalade HAS recognized that it should be paused/terminated
-        // so we can properly return 0.
-        if( s3eDeviceCheckQuitRequest() || s3eDeviceCheckPauseRequest() ) return 0;
+        // Do NOT return 0. It can work, but it will freeze on iOS
+        // when system alarm is started.
+        //return 0;
+        return info->m_NumSamples;
     }
 
     // Copy new data (note: data->preBufferData is a cyclic buffer)
-    dataWritten = min( info->m_NumSamples, data->preBuffer_DataLength );
+    dataWritten = min( info->m_NumSamples, dataLen );
     if( data->preBuffer_DataStartIndex + dataWritten <= data->preBufferSize )
     {
         // Copy all data in 1 step
@@ -204,46 +171,87 @@ int32 s3e_more_audio( void* systemData, void* userData )
         len1 = data->preBufferSize - data->preBuffer_DataStartIndex;
         memcpy( info->m_Target, data->preBufferData + data->preBuffer_DataStartIndex*data->bytesPerSample, len1 * data->bytesPerSample );
         len2 = dataWritten - len1;
-        if( len2 > 0 ) memcpy( info->m_Target, data->preBufferData, len2 * data->bytesPerSample );
+        if( len2 > 0 ) memcpy( info->m_Target + len1 * data->bytesPerSample/2, data->preBufferData, len2 * data->bytesPerSample );
     }
-    data->preBuffer_DataStartIndex += dataWritten;
-    if( data->preBuffer_DataStartIndex >= data->preBufferSize ) data->preBuffer_DataStartIndex -= data->preBufferSize;
-    data->preBuffer_DataLength -= dataWritten;
 
-    data->preBuffer_IsDataReady = 0;
+    // Increse startIndex
+    len1 = data->preBuffer_DataStartIndex + dataWritten;
+    while( len1 >= data->preBufferSize ) len1 -= data->preBufferSize;
+    data->preBuffer_DataStartIndex = len1;
 
-    // Signal to worker thread that it can pre-mix next piece of sound buffer
-    // We can use semaphore in this case, because we will not block this thread
-    // Worker thread will act immediately.
+    // Notify thread that new data is needed
     s3eThreadSemPost(data->thread_semaphore);
 
     return dataWritten;
 }
 
+void    FixForMutedSound( int channelId )
+{
+    // Marmalade fix: play silence (empty buffer) before anything
+    // else, otherwise streaming of sound might not work
+    const int D = 2048;
+    int16 *silenceBuffer, i;
+    int64 timeOut;
+
+    silenceBuffer = (int16*)calloc( 1, sizeof(int16)*D );
+    s3eSoundChannelPlay( channelId, (int16*)silenceBuffer, D/2, 0, 0 );
+    s3eDeviceYield(30);
+    s3eSoundChannelStop( channelId );
+    s3eDeviceYield(10);
+
+    timeOut = s3eTimerGetUST() + 150; // Don't wait more than 150ms
+    // Loop wait while silence sound finishes playing
+    while( s3eSoundChannelGetInt( channelId, S3E_CHANNEL_STATUS ) == 1 )
+    {
+        if( s3eTimerGetUST() > timeOut ) {  // Time out, break
+            timeOut = 0;
+            break;
+        }
+        s3eDeviceYield(5);
+    }
+
+    if( timeOut != 0 ) free(silenceBuffer); // if timed out, don't delete buffer just to be on a safe side
+}
 
 static ALCboolean s3e_open_playback( ALCdevice *device, const ALCchar *deviceName )
 {
-    s3e_data* data;
+    int freeChannel, tries;
+    s3e_data *data;
 
     if( !deviceName ) deviceName = s3eDevice;
     if( strcmp(s3eDevice, deviceName) != 0 ) return ALC_FALSE;
 
+    tries = 0;
+    while( (freeChannel = s3eSoundGetFreeChannel()) == -1 && tries++ < 10 ) {
+        s3eDeviceYield(50);
+    }
+    if( freeChannel == -1 ) return ALC_FALSE;   // Could not set up the channel
+
     data = (s3e_data*)malloc(sizeof(s3e_data));
-    data->channel = s3eSoundGetFreeChannel();
+    data->channel = freeChannel;
     data->mix_data = NULL;
     data->sampleSize = 0;
     data->bytesPerSample = 0;
     data->preBufferData = NULL;
     data->preBufferSize = 0;
     data->preBuffer_DataStartIndex = 0;
-    data->preBuffer_DataLength = 0;
+    data->preBuffer_DataEndIndex = 0;
     data->thread = NULL;
     data->killNow = 0;
     data->thread_semaphore = NULL;
     data->thread_exited = 0;
+
+    FixForMutedSound( data->channel );  // Workaround for Marmalade bug
+
     device->ExtraData = data;
     device->szDeviceName = strdup(deviceName);
     device->FmtType = DevFmtShort;  // 16 bit per channel
+    // when generating sound, channel frequency is ignored - we use output device frequency
+    device->Frequency = s3eSoundGetInt(S3E_SOUND_OUTPUT_FREQ);
+
+    // This is set experimentally - bigger number means less cracking in the noise but slower reaction to sound play/stop
+    // NOTE: Setting this can crash the app when it is resumed from sleep
+    //device->UpdateSize = device->Frequency / 50;
 
     // Register callback functions
     s3eSoundChannelRegister( data->channel, S3E_CHANNEL_GEN_AUDIO, s3e_more_audio, device );
@@ -254,9 +262,6 @@ static ALCboolean s3e_open_playback( ALCdevice *device, const ALCchar *deviceNam
         data->isStereo = s3eSoundChannelRegister( data->channel, S3E_CHANNEL_GEN_AUDIO_STEREO, s3e_more_audio, device ) == S3E_RESULT_SUCCESS;
     } else data->isStereo = 0;
     device->FmtChans = data->isStereo ? DevFmtStereo : DevFmtMono;
-
-    // when generating sound, channel frequency is ignored - we use output device frequency
-    device->Frequency = s3eSoundGetInt(S3E_SOUND_OUTPUT_FREQ);
 
     return ALC_TRUE;
 }
@@ -273,6 +278,8 @@ static void s3e_close_playback( ALCdevice *device )
 static ALCboolean s3e_reset_playback( ALCdevice *device )
 {
     s3e_data *data = (s3e_data*)device->ExtraData;
+    //assert( device->UpdateSize == device->Frequency / 50 );
+
     data->sampleSize = device->UpdateSize;
     data->bytesPerSample = FrameSizeFromDevFmt(device->FmtChans, device->FmtType) * sizeof(ALubyte);
     assert( data->bytesPerSample == (data->isStereo ? 4 : 2) );
@@ -287,10 +294,9 @@ static ALCboolean s3e_reset_playback( ALCdevice *device )
     // Create semaphore we will use to signal worker thread
     data->thread_semaphore = s3eThreadSemCreate(0);
     // Create pre-buffer & initialize data
-    data->preBufferSize = data->sampleSize * 2;
+    data->preBufferSize = data->sampleSize * 3;
     data->preBufferData = (ALubyte*)calloc( 1, data->preBufferSize * data->bytesPerSample );
-    data->preBuffer_DataStartIndex = data->preBuffer_DataLength = 0;
-    data->preBuffer_IsDataReady = 0;
+    data->preBuffer_DataStartIndex = data->preBuffer_DataEndIndex = 0;
 
     // Start worker thread
     data->killNow = 0;
@@ -304,10 +310,10 @@ static ALCboolean s3e_reset_playback( ALCdevice *device )
         s3eThreadSemDestroy(data->thread_semaphore);
         return ALC_FALSE;
     }
+    s3eDeviceYield(20); // Give 20ms to thread to mix first data
 
     // Starting infinite playback cycle with any data
-    //s3eSoundChannelPlay( data->channel, (int16*)data->mix_data, data->sampleSize * data->bytesPerSample, 0, 0 );
-    s3eSoundChannelPlay( data->channel, (int16*)data->mix_data, data->sampleSize * (data->isStereo ? 2 : 1), 0, 0 );
+    s3eSoundChannelPlay( data->channel, (int16*)data->mix_data, data->sampleSize * data->bytesPerSample / 2, 0, 0 );
     return ALC_TRUE;
 }
 
@@ -329,20 +335,24 @@ static void s3e_stop_playback( ALCdevice *device )
         ALvoid *thread = (ALvoid*)data->thread;
         data->thread = NULL;    // Remove the pointer to thread before stopping it
 
+        // Notify thread that something has changed
+        if( data->thread_semaphore ) s3eThreadSemPost(data->thread_semaphore);
+
         // Wait for thread to exit gracefully, but don't block indefinitely
         for( i=0; i<20 && !data->thread_exited; i++ ) Sleep(2);
         // And now really stop the thread
         StopThread( thread );
     }
+    data->preBuffer_DataStartIndex = data->preBuffer_DataEndIndex = 0;
 
     // Destroy the semaphore
     if(data->thread_semaphore != NULL) s3eThreadSemDestroy(data->thread_semaphore);
 
-    s3eSoundChannelStop(data->channel);
     if( data->mix_data != NULL ) {
         free(data->mix_data);
         data->mix_data = NULL;
     }
+
     if( data->preBufferData != NULL ) {
         free(data->preBufferData);
         data->preBufferData = NULL;
